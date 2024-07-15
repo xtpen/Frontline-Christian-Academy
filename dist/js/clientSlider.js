36fcebf4dcbd60c0d92a0e75af9fe4f55f1b3e51
tomatofrog1 <alcosharley21@gmail.com>
2024-07-15 13:33:14 +0800

    tapos na

--- dist/js/clientSlider.js
+++ dist/js/clientSlider.js
@@ -1,9 +1,10 @@
 var slider = tns({
-    container: ".clients__testimonials",
-    items: 1,
-    speed: 100,
-    gutter: 200,
-    slideBy: "page",
-    autoplay: true,
-    navPosition: "bottom",
-  });
+  container: ".clients_wrapper_slider",
+  items: 1,
+  speed: 500,
+  gutter: 200,
+  slideBy: "page",
+  autoplay: true,
+  navPosition: "bottom",
+  controls: false,
+});
